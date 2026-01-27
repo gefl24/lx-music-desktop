567e5d931f4f1695f280a5bed496fa15fbabcb4d
gefl24 <99234064+gefl24@users.noreply.github.com>
2026-01-27 09:56:58 +0800

    Update webpack-build-config.js

--- build-config/webpack-build-config.js
+++ build-config/webpack-build-config.js
@@ -1,3 +1,33 @@
+const path = require('path')
+
 module.exports = {
-  minimize: true,
+  mode: 'production',
+  resolve: {
+    extensions: ['.ts', '.js', '.vue'],
+    alias: {
+      '@': path.resolve(__dirname, '../src'),
+      '@root': path.resolve(__dirname, '../'),
+      '@common': path.resolve(__dirname, '../src/common'),
+      '@main': path.resolve(__dirname, '../src/main'),
+      '@renderer': path.resolve(__dirname, '../src/renderer'),
+      '@server': path.resolve(__dirname, '../src/server'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+  },
 }
