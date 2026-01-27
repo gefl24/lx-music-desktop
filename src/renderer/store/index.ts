0ece6a2c52647519773068ca643cffe113a287a3
gefl24 <99234064+gefl24@users.noreply.github.com>
2026-01-27 10:23:13 +0800

    Update index.ts

--- src/renderer/store/index.ts
+++ src/renderer/store/index.ts
@@ -2,9 +2,16 @@ import { ref, reactive, shallowRef, markRaw, computed, watch } from '@common/uti
 import { windowSizeList as configWindowSizeList } from '@common/config'
 import { appSetting } from './setting'
 import pkg from '../../../package.json'
-import { type ProgressInfo } from 'electron-updater'
 import music from '@renderer/utils/musicSdk'
 process.versions.app = pkg.version
+
+// For web version, we'll define ProgressInfo locally
+type ProgressInfo = {
+  percent: number
+  transferred: number
+  total: number
+  bytesPerSecond: number
+}
 
 export const apiSource = ref<string | null>(null)
 export const proxy: {
