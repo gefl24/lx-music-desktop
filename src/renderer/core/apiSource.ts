7a1435f137744c4b435af0df4b01cfe6ea8f848a
gefl24 <99234064+gefl24@users.noreply.github.com>
2026-01-27 10:21:29 +0800

    Update apiSource.ts

--- src/renderer/core/apiSource.ts
+++ src/renderer/core/apiSource.ts
@@ -1,48 +1,32 @@
 import { apiSource, qualityList, userApi } from '@renderer/store'
 import { appSetting, setApiSource } from '@renderer/store/setting'
-import { setUserApi as setUserApiAction } from '@renderer/utils/ipc'
 import musicSdk from '@renderer/utils/musicSdk'
-import apiSourceInfo from '@renderer/utils/musicSdk/api-source-info'
 
-let prevId = ''
 export const setUserApi = async(apiId: string) => {
-  if (prevId == apiId) return
-  prevId = apiId
-  if (window.lx.apiInitPromise[1]) {
-    window.lx.apiInitPromise[0] = new Promise<boolean>(resolve => {
-      window.lx.apiInitPromise[1] = false
-      window.lx.apiInitPromise[2] = (result: boolean) => {
-        window.lx.apiInitPromise[1] = true
-        resolve(result)
-      }
-    })
+  // For web version, we'll handle API source setting differently
+  console.log('Setting API source:', apiId)
+  
+  // Update the API source in the store
+  apiSource.value = apiId
+  
+  // Update the app setting if needed
+  if (apiId != appSetting['common.apiSource']) {
+    setApiSource(apiId)
   }
-
+  
+  // Initialize quality list and user API state
   if (/^user_api/.test(apiId)) {
     qualityList.value = {}
     userApi.status = false
     userApi.message = 'initing'
-
-    await setUserApiAction(apiId).then(() => {
-      if (prevId != apiId) return
-      apiSource.value = apiId
-    }).catch(err => {
-      if (prevId != apiId) return
-      if (!window.lx.apiInitPromise[1]) window.lx.apiInitPromise[2](false)
-      console.log(err)
-      let api = apiSourceInfo.find(api => !api.disabled)
-      if (!api) return
-      apiSource.value = api.id
-      if (api.id != appSetting['common.apiSource']) setApiSource(api.id)
-    })
+    userApi.apis = {}
   } else {
     // @ts-expect-error
     qualityList.value = musicSdk.supportQuality[apiId] ?? {}
-    apiSource.value = apiId
-    void setUserApiAction(apiId)
-    if (!window.lx.apiInitPromise[1]) window.lx.apiInitPromise[2](true)
   }
-
-  if (prevId != apiId) return
-  if (apiId != appSetting['common.apiSource']) setApiSource(apiId)
+  
+  // Resolve the API init promise
+  if (!window.lx.apiInitPromise[1]) {
+    window.lx.apiInitPromise[2](true)
+  }
 }
