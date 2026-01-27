4658c8e2db63499b17e3206669e5b42a025f855a
gefl24 <99234064+gefl24@users.noreply.github.com>
2026-01-27 10:38:43 +0800

    Update useStatusbarLyric.ts

--- src/renderer/core/useApp/useStatusbarLyric.ts
+++ src/renderer/core/useApp/useStatusbarLyric.ts
@@ -1,16 +1,26 @@
-import { appSetting } from '@renderer/store/setting'
-import { setDisableAutoPauseBySource } from '@renderer/core/lyric'
+// For web version, we'll provide mock implementations
+const appSetting = {
+  'player.isShowStatusBarLyric': false
+}
+
+// Mock function for web version
+const setDisableAutoPauseBySource = () => {
+  console.log('setDisableAutoPauseBySource called')
+}
 
 export default () => {
   const handleEnable = (enable: boolean) => {
     setDisableAutoPauseBySource(enable, 'statusBarLyric')
   }
 
-  window.app_event.on('configUpdate', (setting) => {
-    if (setting['player.isShowStatusBarLyric'] != null) {
-      handleEnable(setting['player.isShowStatusBarLyric'])
-    }
-  })
+  // For web version, we'll mock the app_event
+  if (window.app_event && window.app_event.on) {
+    window.app_event.on('configUpdate', (setting: any) => {
+      if (setting['player.isShowStatusBarLyric'] != null) {
+        handleEnable(setting['player.isShowStatusBarLyric'])
+      }
+    })
+  }
 
   return async() => {
     if (appSetting['player.isShowStatusBarLyric']) {
